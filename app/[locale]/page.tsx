5576f15131a1e0aa49024d465981457e0b741b08
Mogamat-Muneeb <davimuneeb785@gmail.com>
2024-05-06 12:07:47 +0200

    wip

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   const t = useTranslations("Home");
   return (
-    <div>
+    <div className="min-h-screen">
       <h2>{t("title")}</h2>
       <p dangerouslySetInnerHTML={{ __html: t("description") }}></p>
     </div>
